a3f03024d800a958b5a71ecdb9513b6975a3ac7a
Geovanna Alves <132243106+geowzinea@users.noreply.github.com>
2023-10-10 13:51:34 -0300

    Add files via upload

---
+++ desafio5.py
@@ -0,0 +1,9 @@
+#1. Veja no material de apoio o que é a condição if-else
+#2. Faça qualquer programa que declara uma variável e utilize a condição if-else (do jeito que você quiser)
+
+idade = int(input('Qual é a sua idade: '))
+
+if idade >= 18:
+  print("Você é maior de idade")
+else:
+  print('Você é menor de idade')
