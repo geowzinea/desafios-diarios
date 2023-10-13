24a273363b0453505ffc5ca292d78b4b5ed6c3c1
Geovanna Alves <132243106+geowzinea@users.noreply.github.com>
2023-10-13 20:25:43 -0300

    Add files via upload

---
+++ desafio7.py
@@ -0,0 +1,7 @@
+#1. Crie uma função que recebe dois parametros, nome e idade
+#2. Imprima esses valores no formato: "nome, idade"
+
+nome = str(input('Diigite seu nome: '))
+idade = str(input('Digite sua idade: '))
+print(f' nome = {nome} \n idade = {idade}')
+
