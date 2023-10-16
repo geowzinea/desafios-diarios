3f06abe4d579fa968bf66dbb357397ca94b8c594
Geovanna Alves <132243106+geowzinea@users.noreply.github.com>
2023-10-16 15:44:49 -0300

    Add files via upload

---
+++ desafio8.py
@@ -0,0 +1,5 @@
+#1. Agora que você já sabe o que é uma função, crie uma função que receba um parametro em metros e converta para milimetros, ex: 1 metro = 1000 milimetros
+
+metros = int(input('Digite um valor em metros: '))
+mm = metros * 1000
+print(f'O valor de {metros} em milimetros é {mm}')
