26d53132ba3b59e4e678b43347d8ad0989928211
Geovanna Alves <132243106+geowzinea@users.noreply.github.com>
2023-10-16 15:45:05 -0300

    Add files via upload

---
+++ desafio9.py
@@ -0,0 +1,9 @@
+#1. Aprenda a percorrer elementos de um array - veja o material de apoio
+#2. Agora que você já sabe percorrer um array, use o operador de igualdade (==) para verificar verifique se nesse array possui algum número (lembrando que dependendo da linguagem o array pode ter vários tipos de dados)
+
+array = ['Palio', 'Gol', 'Celta', 7, 12.14]
+
+if array == 2:
+  print("O array contém o elemento.")
+else:
+  print("O array não contém o elemento.")
